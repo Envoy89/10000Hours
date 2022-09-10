22f0a712df30992f243d138f4d53f626acb3b6c4
ruzil <ruzil33@gmail.com>
2022-09-10 22:51:08 +0300

    init base spring boot app for server

---
+++ server/src/test/java/com/theruzil/TenThousandHours/TenThousandHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theruzil.TenThousandHours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenThousandHoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
